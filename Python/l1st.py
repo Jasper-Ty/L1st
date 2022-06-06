af6ece70303f2112c047300c1dc39121e59072d9
Jasper Ty <jasperty2@gmail.com>
2022-06-06 00:27:04 -0400

    Added Python implementation

---
+++ Python/l1st.py
@@ -0,0 +1,73 @@
+# TODO: implement list slicing 
+
+class l1st:
+
+    """Python list wrapper that begins indexing at 1"""
+
+    def __init__(self, *args): # args[0] should be an iterable
+        self._list = list(args[0]) if args else list()
+        """L1st instance internal list"""
+    
+    def __getitem__(self, key):
+        return self._list[key-1]
+    
+    def __setitem__(self, key, val):
+        self._list[key-1] = val
+    
+    def __len__(self):
+        return len(self._list)
+
+    def __iter__(self):
+        return iter(self._list)
+    
+    def __contains__(self, item):
+        return item in self._list
+
+    def __repr__(self):
+        return repr(self._list)
+    
+    def __str__(self):
+        return str(self._list)
+
+    def append(self, item):
+        self._list.append(item)
+    
+    def clear(self):
+        self._list.clear()
+    
+    def copy(self):
+        return self._list.copy()
+    
+    def count(self, item):
+        return self._list.count(item)
+
+    def extend(self, iter):
+        self._list.extend(iter)
+    
+    def index(self, value):
+        return self._list.index(value)-1
+    
+    def insert(self, index, item):
+        self._list.insert(index-1, item)
+    
+    def pop(self, *args):
+        idx = args[0] if args else -1
+        self._list.pop(idx)
+    
+    def remove(self, item):
+        self._list.remove(item)
+    
+    def reverse(self):
+        self._list.reverse()
+    
+    def sort(self, **kwargs):
+        self._list.sort(kwargs)
+
+    
+
+l = l1st(i for i in range(4))
+print(l)
+
+print(l[2])
+
+print(*l)
