a837a4b095a321d4cf13d88f2daaa3cdc9e62033
Jasper Ty <jasperty2@gmail.com>
2022-06-06 02:09:10 -0400

    Rewrote L1st.py to utilize inheritance

--- Python/L1st.py
+++ Python/L1st.py
@@ -1,64 +1,25 @@
 """Module which provides a 1-indexed list class named l1st"""
 
-class l1st:
+class l1st(list):
 
     """Python list wrapper that begins indexing at 1"""
-
-    def __init__(self, *args): # args[0] should be an iterable
-        self._list = list(args[0]) if args else list()
-        """L1st instance internal list"""
     
+    def __init__(self, *args):
+        super().__init__(*args)
+
     def __getitem__(self, key):
-        return self._list[key-1]
+        return super().__getitem__(key-1)
     
     def __setitem__(self, key, val):
-        self._list[key-1] = val
-    
-    def __len__(self):
-        return len(self._list)
+        super().__setitem__(key-1, val)
 
-    def __iter__(self):
-        return iter(self._list)
-    
-    def __contains__(self, item):
-        return item in self._list
-
-    def __repr__(self):
-        return repr(self._list)
-    
-    def __str__(self):
-        return str(self._list)
-
-    def append(self, item):
-        self._list.append(item)
-    
-    def clear(self):
-        self._list.clear()
-    
-    def copy(self):
-        return self._list.copy()
-    
-    def count(self, item):
-        return self._list.count(item)
-
-    def extend(self, iter):
-        self._list.extend(iter)
-    
     def index(self, value):
-        return self._list.index(value)+1
+        return super().index(value)+1
     
     def insert(self, index, item):
-        self._list.insert(index-1, item)
-    
+        super().insert(index-1, item)
+
     def pop(self, *args):
-        idx = args[0] if args else -1
-        self._list.pop(idx)
+        idx = args[0]-1 if args else -1
+        super().pop(idx)
     
-    def remove(self, item):
-        self._list.remove(item)
-    
-    def reverse(self):
-        self._list.reverse()
-    
-    def sort(self, **kwargs):
-        self._list.sort(kwargs)
